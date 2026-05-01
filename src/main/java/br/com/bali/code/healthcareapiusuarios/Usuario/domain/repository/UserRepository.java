1074a04185ee183002f67efc144e90a6032e0e14
Luis <banedycraft@gmail.com>
2026-05-01 01:57:10 -0300

    user repository

--- src/main/java/br/com/bali/code/healthcareapiusuarios/Usuario/domain/repository/UserRepository.java
+++ src/main/java/br/com/bali/code/healthcareapiusuarios/Usuario/domain/repository/UserRepository.java
@@ -4,7 +4,12 @@ import br.com.bali.code.healthcareapiusuarios.Usuario.domain.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import br.com.bali.code.healthcareapiusuarios.Usuario.application.Role;
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Long>  {
-
+    Optional<User> findByEmail(String email);
+    List<User> findByRole(Role role);
 }
